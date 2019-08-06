8765fb2c9a7240d42d1291d02b331247a254c8e9
边境 <bianjing@heqi-inc.com>
2019-08-06 19:38:07 +0800

    总体流程更新

--- run.py
+++ run.py
@@ -2,13 +2,24 @@ from view import app
 import time
 import logging
 
+
+def config_logger():
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    fh = logging.FileHandler('news.log', mode='w')
+    fh.setLevel(logging.INFO)
+    formatter = logging.Formatter("%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s")
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+    # 日志
+    logger.debug('this is a logger debug message')
+    logger.info('this is a logger info message')
+    logger.warning('this is a logger warning message')
+    logger.error('this is a logger error message')
+    logger.critical('this is a logger critical message')
+
+
 if __name__ == "__main__":
-    # handler = logging.FileHandler('news.log')
-    # app.logger.addHandler(handler)
-    # logger = logging.getLogger('/var/log/nlp/nlp_classes.log')
-    # logger.setLevel(logging.DEBUG)
-    # formatter = '%(asctime)s: %(levelname)s %(filename)s-%(module)s-%(funcName)s-%(lineno)d %(message)s'
-    # log_formatter = logging.Formatter(formatter)
-    # handler = logging.FileHandler('/var/log/nlp/nlp_classes.log')
-    # logger.addHandler(handler)
+    # 设置日志
+    config_logger()
     app.run(port=9999)
